8cef26c96ca0a17c24729b201d458e9bd6f7c0fd
Dong7786 <League@AdministorsiMac.attlocal.net>
2019-05-02 18:29:32 -0700

    Changes :smile:
    More changes

--- src/extra/StarShow.java
+++ src/extra/StarShow.java
@@ -15,7 +15,7 @@ public class StarShow {
 		
 		drawStar(150);	//5. delete this line. you will draw the star again in step 8.
 		// 13. Set the speed to 8
-
+		robot.setSpeed(8);
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
 
 		// 7. Make a variable to hold the Y position of the Robot and set it to 600
@@ -46,11 +46,16 @@ public class StarShow {
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+		robot.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
 
 			// 1. Move the robot the distance of the starSize variable
-	
+		for (int i = 0; i < 5; i++) {
+			
+		
+			robot.move(starSize);
+			robot.turn(144);
+		}
 			// 3. Turn the robot 144 degrees
 			
 	}
